bb88e57a0693a6ad15639a7dd0bdc812f9455a6d
book <bookgvi@gmail.com>
2020-01-24 00:56:58 +0300

    formatting

--- src/App.vue.js
+++ src/App.vue.js
@@ -1,14 +1,15 @@
-import * as tslib_1 from "tslib";
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import * as tslib_1 from 'tslib'
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
+
 let App = class App extends Vue {
-};
+}
 App = tslib_1.__decorate([
-    Component({
-        components: {
-            HelloWorld
-        }
-    })
-], App);
-export default App;
+  Component({
+    components: {
+      HelloWorld
+    }
+  })
+], App)
+export default App
 //# sourceMappingURL=App.vue.js.map
